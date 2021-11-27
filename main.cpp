62074b661cf4fd091910f6dbac4ff8fe6c3770c6
santo-orl <orlandosantino11@gmail.com>
2021-11-27 13:31:05 -0500

    listener
    aggiunto thread che sta in ascolto su localhost:8080

--- main.cpp
+++ main.cpp
@@ -9,8 +9,23 @@
 #include <cpp-httplib-master/httplib.h>
 #include <QDesktopServices>
 #include <QUrl>
+#include <thread>
 
 using namespace curlpp::options;
+
+
+//funzione che viene passata al thread per essere eseguita
+void listener(){
+    std::cout<<"server on"<<std::endl;
+    httplib::Server svr;
+    svr.Get("/", [](const httplib::Request & req, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+        std::cout<< req.get_param_value("code") <<std::endl;
+    });
+    std::cout<<"server listening.."<<std::endl;
+    svr.listen("127.0.0.1", 8080);
+}
+
 
 int main(int argc, char *argv[]){
 
@@ -22,16 +37,8 @@ int main(int argc, char *argv[]){
     //apre il browser con il link passato
     QDesktopServices::openUrl(QUrl("https://accounts.google.com/o/oauth2/v2/auth?client_id=1034786735866-o2d6ot6i3mvvrvt2rck9qent1a32odnb.apps.googleusercontent.com&response_type=code&scope=https://www.googleapis.com/auth/calendar&redirect_uri=http://localhost:8080&code_challenge=1111111111111111111111111111111111111111111&challenge_method=plain"));
 
-    //creo un server su localhost:8080
-    httplib::Server svr;
+    std::thread t1{listener};
+    t1.join();
 
-    //faccio una get quando qualcuno contatta localhost:8080 e prelevo il valore 'code'
-    //che contiene il codice da allegare alla post per ottenere l'auth code
-    svr.Get("/", [](const httplib::Request & req, httplib::Response &res) {
-        res.set_content("Hello World!", "text/plain");
-        std::cout<< req.get_param_value("code") <<std::endl;
-    });
 
-    //mi metto in ascolto su localhost 8080
-    svr.listen("127.0.0.1", 8080);
 }
